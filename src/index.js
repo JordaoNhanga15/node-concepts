a830696e0f529dcf11d5f97dc20663dd3e93ee43
Jordão Oliveira <jordao.oliveira@Admins-MacBook-Pro.local>
2022-11-27 02:23:23 +0100

    feat: desafio 2

--- src/index.js
+++ src/index.js
@@ -1,41 +1,107 @@
 const express = require('express');
 const cors = require('cors');
-
-// const { v4: uuidv4 } = require('uuid');
+const { v4: uuidv4 } = require('uuid');
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
-// const users = [];
+const users = [];
 
-function checksExistsUserAccount(request, response, next) {
-  // Complete aqui
+function checksExistsUserAccount(req, res, next) {
+  const { username } = req.headers;
+
+  const user = users.find((user) => user.username === username);
+
+  if (!user) return res.status(404).json({ error: 'User not found' });
+
+  req.user = user;
+
+  return next();
 }
 
-app.post('/users', (request, response) => {
-  // Complete aqui
+app.post('/users', (req, res) => {
+  const { name, username } = req.body;
+
+  const userAlreadyExists = users.some((user) => user.username === username);
+
+  if (userAlreadyExists) return res.status(400).json({ error: 'User already exists!' });
+
+  const user = {
+    id: uuidv4(),
+    name,
+    username,
+    todos: []
+  }
+
+  users.push(user);
+
+  return res.status(201).json(user);
 });
 
-app.get('/todos', checksExistsUserAccount, (request, response) => {
-  // Complete aqui
+app.get('/todos', checksExistsUserAccount, (req, res) => {
+  const { user } = req;
+
+  return res.json(user.todos);
 });
 
-app.post('/todos', checksExistsUserAccount, (request, response) => {
-  // Complete aqui
+app.post('/todos', checksExistsUserAccount, (req, res) => {
+  const { user } = req;
+  const { title, deadline } = req.body;
+
+  const todo = {
+    id: uuidv4(),
+    title,
+    done: false,
+    deadline: new Date(deadline),
+    created_at: new Date()
+  }
+
+  user.todos.push(todo);
+
+  return res.status(201).json(todo);
 });
 
-app.put('/todos/:id', checksExistsUserAccount, (request, response) => {
-  // Complete aqui
+app.put('/todos/:id', checksExistsUserAccount, (req, res) => {
+  const { user } = req;
+  const { id } = req.params;
+  const { title, deadline } = req.body;
+
+  const todo = user.todos.find((todo) => todo.id === id);
+
+  if (!todo) return res.status(404).json({ error: 'Todo not found' });
+
+  todo.title = title;
+  todo.deadline = new Date(deadline);
+
+  return res.json(todo);
 });
 
-app.patch('/todos/:id/done', checksExistsUserAccount, (request, response) => {
-  // Complete aqui
+app.patch('/todos/:id/done', checksExistsUserAccount, (req, res) => {
+  const { user } = req;
+  const { id } = req.params;
+
+  const todo = user.todos.find((todo) => todo.id === id);
+
+  if (!todo) return res.status(404).json({ error: 'Todo not found' });
+
+  todo.done = true;
+
+  return res.json(todo);
 });
 
-app.delete('/todos/:id', checksExistsUserAccount, (request, response) => {
-  // Complete aqui
+app.delete('/todos/:id', checksExistsUserAccount, (req, response) => {
+  const { user } = req;
+  const { id } = req.params;
+
+  const todo = user.todos.find((todo) => todo.id === id);
+
+  if (!todo) return response.status(404).json({ error: 'Todo not found' });
+
+  user.todos.splice(todo, 1);
+
+  return response.status(204).send();
 });
 
 module.exports = app;
--- src/server.js
+++ src/server.js
@@ -1,3 +1,9 @@
 const app = require('./');
+const kleur = require('kleur');
+const dotenv = require('dotenv');
+dotenv.config();
 
-app.listen(3333);
+
+app.listen(process.env.PORT, () => {
+  console.log(kleur.green().bold(`Server is running in ${process.env.PORT}!`));
+});
